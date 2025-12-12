05df838cb83dc028be69072a9946f10127e4fc44
Antonio Fratamico <fratamicoa@outlook.com>
2025-12-12 14:29:54 +0000

    Update test-build_dependency_map.R

--- tests/testthat/test-build_dependency_map.R
+++ tests/testthat/test-build_dependency_map.R
@@ -2,26 +2,22 @@
 library(testthat)
 library(functiondepends)
 
-test_that("build_dependency_map correctly maps function dependencies using functiondepends", {
-  
-  # Create a temporary environment
-  temp_env <- new.env()
+test_that("build_dependency_map handles simple dependencies", {
+  env <- new.env()
+  env$a <- function() b()
+  env$b <- function() c()
+  env$c <- function() NULL
 
-  # Define mock functions in the temporary environment
-  temp_env$func_c <- function() {}
-  temp_env$func_b <- function() { func_c() }
-  temp_env$func_a <- function() { func_b(); func_c() }
+  dep_map <- build_dependency_map("a", env)
+  expect_true(is.list(dep_map))
+  expect_equal(names(dep_map), c("a", "b", "c"))
+  expect_equal(dep_map[["a"]], "b")
+  expect_equal(dep_map[["b"]], "c")
+})
 
-  # Assign them to the global environment
-  assign("func_a", temp_env$func_a, envir = temp_env)
-  assign("func_b", temp_env$func_b, envir = temp_env)
-  assign("func_c", temp_env$func_c, envir = temp_env)
-
-  # Run the function
-  dep_map <- build_dependency_map("func_a", env = temp_env)
-
-  # Check that the result is a list and contains expected function names
-  expect_type(dep_map, "list")
-  expect_true(all(c("func_a", "func_b", "func_c") %in% names(dep_map)))
-
+test_that("build_dependency_map handles missing function", {
+  env <- new.env()
+  env$a <- function() missing_fn()
+  dep_map <- build_dependency_map("a", env)
+  expect_equal(dep_map[["a"]], "missing_fn")
 })
